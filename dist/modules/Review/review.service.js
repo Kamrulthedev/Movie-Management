4a2576a9a6b661a1f763f05242ccbfd7093b2991
Kamrul Hassan <kamrulthedev@gmail.com>
2024-06-07 18:04:03 +0600

    review add commit

--- dist/modules/Review/review.service.js
+++ dist/modules/Review/review.service.js
@@ -13,16 +13,30 @@ exports.ReviewServices = void 0;
 const movie_model_1 = require("../Movies/movie.model");
 const review_model_1 = require("./review.model");
 const addReview = (slug, reviewData) => __awaiter(void 0, void 0, void 0, function* () {
+    const session = yield movie_model_1.Movie.startSession();
     const movie = yield movie_model_1.Movie.findOne({ slug });
     if (!movie) {
-        throw new Error("Movie Not Found !!");
+        throw new Error("Movie not found");
     }
-    const review = yield review_model_1.Review.create(Object.assign({ movie: movie._id }, reviewData));
-    const reviewCount = yield review_model_1.Review.countDocuments({
-        movie: movie._id,
-    });
-    yield movie_model_1.Movie.updateOne({ slug }, { totalRating: reviewCount }, { new: true });
-    return review;
+    try {
+        session.startTransaction();
+        const review = yield review_model_1.Review.create([
+            Object.assign({ movie: movie._id }, reviewData),
+        ], { session });
+        const reviewsCount = yield review_model_1.Review.countDocuments({
+            movie: movie._id,
+        }).session(session);
+        // throw new Error("Movie not found");
+        yield movie_model_1.Movie.updateOne({ slug }, { totalRating: reviewsCount }, { session });
+        yield session.commitTransaction();
+        return review[0];
+    }
+    catch (error) {
+        console.log(error);
+        yield session.abortTransaction();
+        throw error;
+    }
+    session.endSession();
 });
 exports.ReviewServices = {
     addReview,
--- src/modules/Review/review.service.ts
+++ src/modules/Review/review.service.ts
@@ -3,24 +3,49 @@ import { TReview } from "./review.interface";
 import { Review } from "./review.model";
 
 const addReview = async (slug: string, reviewData: Partial<TReview>) => {
+  
+  const session = await Movie.startSession();
+
   const movie = await Movie.findOne({ slug });
 
   if (!movie) {
-    throw new Error("Movie Not Found !!");
+    throw new Error("Movie not found");
   }
-  const review = await Review.create({
-    movie: movie._id,
-    ...reviewData,
-  });
 
-  const reviewCount = await Review.countDocuments({
-    movie: movie._id,
-  });
+  try {
+    session.startTransaction();
 
-  await Movie.updateOne({ slug }, { totalRating: reviewCount }, { new: true });
+    const review = await Review.create(
+      [
+        {
+          movie: movie._id,
+          ...reviewData,
+        },
+      ],
+      { session }
+    );
 
-  return review;
+    const reviewsCount = await Review.countDocuments({
+      movie: movie._id,
+    }).session(session);
+
+    // throw new Error("Movie not found");
+
+    await Movie.updateOne({ slug }, { totalRating: reviewsCount }, { session });
+
+    await session.commitTransaction();
+
+    return review[0];
+  } catch (error) {
+    console.log(error);
+    await session.abortTransaction();
+    throw error;
+  }
+
+  session.endSession();
 };
+
+
 
 export const ReviewServices = {
   addReview,
