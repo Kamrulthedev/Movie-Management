20873dbdb28f42bfc31a7b19a8346becba680235
Kamrul Hassan <kamrulthedev@gmail.com>
2024-06-16 16:21:27 +0600

    User updated successfully

--- dist/app.js
+++ dist/app.js
@@ -30,9 +30,11 @@ const express_1 = __importStar(require("express"));
 const movie_route_1 = require("./modules/Movies/movie.route");
 const NotFound_1 = __importDefault(require("./Middleware/NotFound"));
 const GolobalErrorHandlar_1 = __importDefault(require("./Middleware/GolobalErrorHandlar"));
+const user_route_1 = require("./modules/Users/user.route");
 const app = (0, express_1.default)();
 app.use((0, express_1.json)());
 app.use('/api/movies', movie_route_1.MovieRouter);
+app.use("/api/users", user_route_1.UserRoutes);
 app.get('/', (req, res) => {
     res.send('Hello Next!');
 });
--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,13 @@ import express, { Request, Response, json } from "express";
 import { MovieRouter } from "./modules/Movies/movie.route";
 import NotFound from "./Middleware/NotFound";
 import golobalErrorhandlar from "./Middleware/GolobalErrorHandlar";
+import { UserRoutes } from "./modules/Users/user.route";
 const app = express()
 
 app.use(json())
 app.use('/api/movies', MovieRouter)
+
+app.use("/api/users", UserRoutes)
 
 app.get('/', (req: Request, res:Response) => {
   res.send('Hello Next!')
